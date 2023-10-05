067175cd3eb733a7cad88d29e06cd06d3126a7f1
Josh Pablico <jbpablico27@gmail.com>
2023-10-05 16:16:56 -0500

    JS

---
+++ src/script/script.js
@@ -0,0 +1,18 @@
+const gameSetup = (function() {
+	'use strict';
+  
+	const gameboardContainer = document.getElementsByClassName("gameboard-container")[0];
+	
+	const GameBoard = (() => {
+		const gameboard = Array[9];
+
+		gameboard.array.forEach(element => {
+			square.
+		});
+	})();
+	return {
+		publicMethod: function() {
+		  
+		}
+	};
+  })();
