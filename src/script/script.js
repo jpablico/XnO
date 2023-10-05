4699d20dbcbae1404644b39de9675fa4c91583ae
Josh Pablico <jbpablico27@gmail.com>
2023-10-05 15:51:06 -0500

    JS

--- src/script/script.js
+++ src/script/script.js
@@ -3,8 +3,39 @@ const gameXnO = (function() {
 
 	const GameBoard = (() => {
 		const board = ["", "", "", "", "", "", "", "", ""];
-			
-	})
+
+		                                                             
+	})();
+
+	const Player = (name, mark) => {
+
+	};
+
+	function renderGameBoard() {
+
+	}
+
+	function handlePlayerMove(cellIndex) {
+
+	}
+
+	function checkGameOver() {
+		const gameWinningConditions = [
+			[0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+		];
+	}
+
+	function setupGame() {
+
+	}
+
 
 })();
 /*
